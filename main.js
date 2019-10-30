18fb2f703441f29c1f959dad3045d93a1ec2b408
bran.yeom <bran.yeom@kakaocorp.com>
2019-10-30 14:32:31 +0900

    Lesson 5 End

--- main.js
+++ main.js
@@ -23,9 +23,12 @@ var app = new Vue({
     addToCart() {
       this.cart += 1
     },
+    removeFromCart() {
+      if(this.cart > 0)
+        this.cart -= 1
+    },
     updateProduct(variantImage) {
       this.image = variantImage
-
-    }
+    },
   }
 });
