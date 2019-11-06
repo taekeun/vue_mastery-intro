4462c8cf49bb55a117eff8745eacdd6d1f3c87b4
bran.yeom <bran.yeom@kakaocorp.com>
2019-11-06 21:02:28 +0900

    props

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 Vue.component('product', {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true
+    }
+  },
   template: `
     <div class="product">
       <div class="product-image">
@@ -9,6 +15,7 @@ Vue.component('product', {
         <h1>{{ title }}</h1>
         <p v-if="inStock">In Stock</p>
         <p v-else :class="{outOfStock: !inStock}">Out of Stock</p>
+        <p>User is premium: {{ premium }}</p>
 
         <ul>
           <li v-for="detail in details">{{ detail }}</li>
@@ -83,4 +90,7 @@ Vue.component('product', {
 
 var app = new Vue({
   el: '#app',
+  data: {
+    premium: true
+  }
 });
