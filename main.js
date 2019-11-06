d9574178942a42aab2d48288303df5fbcde80c8e
bran.yeom <bran.yeom@kakaocorp.com>
2019-11-06 21:10:40 +0900

    Lesson 8 End component inside component

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+Vue.component('product-details', {
+  props: {
+    details: {
+      type: Array,
+      required: true
+    }
+  },
+  template: `
+    <ul>
+      <li v-for="detail in details">{{ detail }}</li>
+    </ul>
+  `
+
+})
+
 Vue.component('product', {
   props: {
     premium: {
@@ -17,9 +32,7 @@ Vue.component('product', {
         <p v-else :class="{outOfStock: !inStock}">Out of Stock</p>
         <p>Shipping: {{ shipping }}</p>
 
-        <ul>
-          <li v-for="detail in details">{{ detail }}</li>
-        </ul>
+        <product-details :details="details"></product-details>
 
         <div class="color-box"
              v-for="(variant, index) in variants"
